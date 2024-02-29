0c90ba94a8a3b1a48868b04295e12dbfcc9523e8
Mike Burke <mike.burke@recyclinglives.com>
2024-02-29 17:20:42 +0000

    Responsive layout tweak for mobile view

--- resources/views/livewire/create-task.blade.php
+++ resources/views/livewire/create-task.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <form wire:submit.prevent="save">
+    <form wire:submit.prevent="save" class="mb-8">
         <div class="form-group {{ $errors->get('task') ? 'has-error' : '' }}">
             <x-text-input class="w-full" wire:model="task" id="task" type="text" name="task" placeholder="Insert task name" required autofocus />
             <x-input-error :messages="$errors->get('task')" class="mt-4" />
